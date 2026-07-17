e7cc0b31a9e551adaf4edf8f8b0d88e0184efe98
Alex Ghit <76708587+alexghit@users.noreply.github.com>
2026-07-17 12:05:20 +0300

    Delete worker.js

--- worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const res = await env.ASSETS.fetch(request);
-    if (res.status !== 404) return res;
-    return Response.redirect(new URL('/', request.url), 301);
-  }
-};
